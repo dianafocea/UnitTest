ee4043b4c4d9322c34653fc02594662354aab150
dianafocea <diana.focea13@gmail.com>
2023-01-25 08:52:15 +0200

    +

--- unit_tests/test_cont.py
+++ unit_tests/test_cont.py
@@ -1,11 +1,11 @@
 from app.cont import Cont
 
 
-class TestCont():
+class TestCont:
 
     def setup_method(self):
         print("Se executa la inceput")
-        self.cont = Cont('RO123456', 'Mircea', 50000)
+        self.cont = Cont('RO123456', 'Mitrescu Mircea', 50000)
 
 
     def teardown_method(self):
@@ -13,14 +13,14 @@ class TestCont():
 
 
     def test_afisare_sold(self):
-        assert self.cont.afisare_sold() == "Titularul `Mircea` are in contul RO123456 " \
+        assert self.cont.afisare_sold() == "Titularul `Mitrescu Mircea` are in contul RO123456 " \
         "suma de 50000 £", 'Eroare afisare_sold'
 
     def test_creditare_cont(self):
         assert self.cont.creditare_cont(1000) == "Ati adaugat 1000 £, sold disponibil: 51000 £",\
-        'Error, salariul lunar nu corespunde'
+        'Error, nu s-a putut credita suma'
 
     def test_debitare_cont(self):
         assert self.cont.debitare_cont(8000) == "Ati retras 8000 £, sold disponibil: 42000 £",\
-        'Error, suma nu corespunde'
+        'Error, fonduri insuficiente'
 
